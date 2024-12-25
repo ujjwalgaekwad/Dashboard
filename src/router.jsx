3f55730bc9fbb3c15fd118559993e09ad6d284e3
ujjwal singh <your-ujjwalsinghgaekwad@gmail.com>
2024-12-25 17:39:19 +0530

    add router dom

---
+++ src/router.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home/>
+    },
+    {
+        path: "/login",
+        element: <Login/>
+    },
+]);
+
+export default router;
