0ce49377f8ba30d7fa7111bc2f1860a43ebef49c
ujjwal singh <your-ujjwalsinghgaekwad@gmail.com>
2024-12-25 20:53:49 +0530

    add login page

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,61 @@
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardHeader,
+    CardDescription,
+    CardTitle
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Link } from "react-router-dom"
+
+
+function LoginPage() {
+    return (
+        <section className="flex justify-center items-center h-screen w-screen">
+            <div className="flex flex-col gap-6">
+            <Card>
+                <CardHeader>
+                    <CardTitle className="text-2xl">Login</CardTitle>
+                    <CardDescription>
+                         Enter your email below to login to your account
+                    </CardDescription> 
+                </CardHeader>
+                <CardContent>
+                    <form>
+                        <div className="flex flex-col gap-6">
+                            <div className="grid gap-2">
+                                <Label htmlFor="email">Email</Label>
+                                <Input
+                                    id="email"
+                                    type="email"
+                                    placeholder="m@example.com"
+                                    required
+                                />
+                            </div>
+                            <div className="grid gap-2">
+                                <div className="flex items-center">
+                                    <Label htmlFor="password">Password</Label>
+                                </div>
+                                <Input id="password" type="password" required />
+                            </div>
+                            <Button type="submit" className="w-full">
+                                Login
+                            </Button>
+                        </div>
+                        <div className="mt-4 text-center text-sm">
+                            Don&apos;t have an account?{" "}
+                            <Link to={"/register"} className="underline underline-offset-4">
+                                Sign Up
+                            </Link>
+                        </div>
+                    </form>
+                </CardContent>
+            </Card>
+        </div>
+        </section>
+    )
+}
+
+export default LoginPage;
